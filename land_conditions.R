e02661a9bb2b2ec43bb38d8dace44937c02ff73f
Hayden Waters <haydenrwaters@gmail.com>
2026-05-29 14:27:07 -0700

    change variable names

--- land_conditions.R
+++ land_conditions.R
@@ -3,9 +3,9 @@ library(here)
 
 land_conditions = function(fuel_moisture, slope) {
 
-  fuel_ignition_prob = 1 / (1 + exp(0.64*fuel_moisture - 3.92)) #Estimate fuel ignition probability using a logistic style curve from Lawson et al. 
+  fuel_ignition_probability = 1 / (1 + exp(0.64*fuel_moisture - 3.92)) #Estimate fuel ignition probability using a logistic style curve from Lawson et al. 
   slope_spread_factor = 2**(slope/10) #Incorporate the effect of slope on the rate of spread. Rate of spread doubles for every 10 degrees of slope. 
-  land_fire_prob = fuel_ignition_prob * slope_spread_factor
-  return(land_fire_prob)
+  land_fire_probability = fuel_ignition_probability * slope_spread_factor
+  return(land_fire_probability)
 }
 
