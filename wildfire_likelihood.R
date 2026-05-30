88b6d48e1377ca40deefa4cbe84152e83965b507
Hayden Waters <haydenrwaters@gmail.com>
2026-05-29 18:14:23 -0700

    Change climate fire probability name

--- wildfire_likelihood.R
+++ wildfire_likelihood.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(here)
 
-wildfire_likelihood = function(climate_risk_factor, land_fire_probability) {
+wildfire_likelihood = function(climate_fire_probability, land_fire_probability) {
   wildfire_probability = climate_risk_factor*land_fire_probability
 
   return(wildfire_probability)
