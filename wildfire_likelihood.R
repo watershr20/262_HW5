ab599e2cebe2926e581e9167f720649f0d226920
christinatreacy <ctreacy@bren.ucsb.edu>
2026-05-30 00:13:19 -0700

    again

--- wildfire_likelihood.R
+++ wildfire_likelihood.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(here)
 
 wildfire_likelihood = function(climate_fire_probability, land_fire_probability) {
-  wildfire_probability = climate_risk_probability * land_fire_probability
+  wildfire_probability = climate_fire_probability * land_fire_probability
 
   return(wildfire_probability)
 }
